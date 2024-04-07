a8884255cc76160cc1c542fa3d8517cfa9f77f82
ethan-hyatt <hyatt.b.ethan@gmail.com>
2024-04-07 19:45:44 -0400

    added search by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,13 +18,26 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn])
+    res.send(JSON.stringify(books[isbn]),null,4)
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    //console.log(author);
+    let foundbooks = [];
+    let bookskeys = Object.keys(books);
+    for (const key in bookskeys) {
+        let book = books[bookskeys[key]];
+        //console.log(book);
+        //console.log(bookskeys[key]);
+        //console.log(book["author"]);
+        if (book["author"] === author) {
+            foundbooks.push(bookskeys[key],book)
+        }
+    }
+    //console.log(foundbooks);
+    res.send(foundbooks)
 });
 
 // Get all books based on title
