c471c6ce301ed1276ae89402bb9db1a4797a39b2
ethan-hyatt <hyatt.b.ethan@gmail.com>
2024-04-08 03:54:01 -0400

    added promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,46 +32,59 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+    new Promise((resolve, reject) => {
+        res.send(JSON.stringify(books,null,4));
+        resolve();
+    });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn]),null,4)
+    let myPromise = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        resolve(isbn);
+    });
+    myPromise.then((isbn) => {
+        res.send(JSON.stringify(books[isbn]),null,4);
+    });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    //console.log(author);
-    let foundbooks = [];
-    let bookskeys = Object.keys(books);
-    for (const key in bookskeys) {
-        let book = books[bookskeys[key]];
-        //console.log(book);
-        //console.log(bookskeys[key]);
-        //console.log(book["author"]);
-        if (book["author"] === author) {
-            foundbooks.push(bookskeys[key],book)
+    let myPromise = new Promise((resolve, reject) => {
+        const author = req.params.author;
+        var foundbooks = [];
+        var bookskeys = Object.keys(books);
+        for (const key in bookskeys) {
+            var book = books[bookskeys[key]];
+            if (book["author"] === author) {
+                foundbooks.push(bookskeys[key],book)
+            }
         }
-    }
-    //console.log(foundbooks);
-    res.send(foundbooks)
+        resolve(foundbooks);
+    });
+    myPromise.then((foundbooks) => {
+        res.send(foundbooks);
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  let foundbooks = [];
-    let bookskeys = Object.keys(books);
-    for (const key in bookskeys) {
-        let book = books[bookskeys[key]];
-        if (book["title"] === title) {
-            foundbooks.push(bookskeys[key],book)
+    let myPromise = new Promise((resolve, reject) => {
+        const title = req.params.title;
+        let foundbooks = [];
+        let bookskeys = Object.keys(books);
+        for (const key in bookskeys) {
+            let book = books[bookskeys[key]];
+            if (book["title"] === title) {
+                foundbooks.push(bookskeys[key],book)
+            }
         }
-    }
-    res.send(foundbooks)
+        resolve(foundbooks);
+    });
+    myPromise.then((foundbooks) => {
+        res.send(foundbooks);
+    });
 });
 
 //  Get book review
