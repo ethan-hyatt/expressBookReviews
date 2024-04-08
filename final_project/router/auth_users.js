fb2716982d66a8c6a464c0b03bd051c50a36262f
ethan-hyatt <hyatt.b.ethan@gmail.com>
2024-04-08 00:37:20 -0400

    added book review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,19 +10,67 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+      });
+      if(validusers.length > 0){
+        return true;
+      } else {
+        return false;
+      }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+          return res.status(404).json({message: "Error logging in"});
+    }
+
+    if (authenticatedUser(username,password)) {
+        let accessToken = jwt.sign({
+          data: password
+        }, 'access', { expiresIn: 60 * 60 });
+    
+        req.session.authorization = {
+          accessToken,username
+      }
+      return res.status(200).send("User successfully logged in");
+      } else {
+        return res.status(208).json({message: "Invalid Login. Check username and password"});
+      }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const review = req.body.review;
+  const username = req.body.username;
+  const isbn = req.params.isbn;
+  let bookreviews = books[isbn]["reviews"];
+  console.log(bookreviews);
+  console.log(review);
+  let alreadyreviewed = false
+  for (const reviews in bookreviews) {
+    console.log(reviews);
+    if (bookreviews[reviews]["username"] === username) {
+        books[isbn]["reviews"][reviews]["review"] = review;
+        alreadyreviewed = true;
+        res.send("Review modified\n" + JSON.stringify(books[isbn]));
+    }
+  }
+  if (!alreadyreviewed) {
+    let newReview = {"review": review, "username": username}
+    if (Object.keys(bookreviews).length === 0) {
+        books[isbn]["reviews"] = [newReview];
+    }
+    else {
+        books[isbn]["reviews"].push(newReview);
+    }
+    console.log(books[isbn]);
+    res.send("Review added\n"+ JSON.stringify(books[isbn]));
+  }
 });
 
 module.exports.authenticated = regd_users;
